239ed3a208811dffee33376f2b0864ef7ef75db9
Sergey <bondarevsv36@gmail.com>
2025-04-30 17:24:34 +0300

    add function get_mask_account

--- src/widget.py
+++ src/widget.py
@@ -3,4 +3,6 @@ from masks import get_mask_account
 def mask_account_card(account_card: str):
     name, number = account_card.rsplit(' ', 1)
     if name == 'Счет':
-        return name + get_mask_account(number)
+        return name + ' ' + get_mask_account(number)
+
+print(mask_account_card('Счет 64686473678894779589'))
