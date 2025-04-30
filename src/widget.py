2a8f285e10bbc88ae4cea786a80fc32f08d01f12
Sergey <bondarevsv36@gmail.com>
2025-04-30 18:21:41 +0300

    add get_date function

--- src/widget.py
+++ src/widget.py
@@ -13,3 +13,12 @@ def mask_account_card(account_card: str) -> str:
         mask = name + " " + get_mask_card_number(number)
 
     return mask
+
+def get_date(date_string: str) -> str:
+    date_need = date_string[:10]
+    date_list = []
+    date_list = date_need.split('-')
+    sort_date_list = date_list[::-1]
+    return '.'.join(sort_date_list)
+
+print(get_date("2024-03-11T02:26:18.671407"))
