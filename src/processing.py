803e62465343f0a4da107b506a0452a79fae00f4
Sergey <bondarevsv36@gmail.com>
2025-05-02 13:44:02 +0300

    add docstring, annotation and check lint test processing module

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,28 @@
-from widget import get_date
-
-def filter_by_state(filter_list: list[dict], state='EXECUTED'):
+def filter_by_state(filter_list: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """
+    Функция принимает список словарей
+    и опционально значение для ключа state (по умолчанию 'EXECUTED').
+    Функция возвращает новый список словарей,
+    содержащий только те словари, у которых ключ state соответствует указанному значению
+    :param filter_list: список словарей
+    :param state: параметр фильтра (по умолчанию 'EXECUTED')
+    :return: отфильтрованный список словарей
+    """
     new_list_dict = []
     for i_dict in filter_list:
-        if i_dict['state'] == state:
+        if i_dict["state"] == state:
             new_list_dict.append(i_dict)
     return new_list_dict
 
-def sort_by_date(sort_list, sort_key=False):
-    sorted_list = sorted(sort_list, key=lambda sorted_key: sorted_key['date'][:10], reverse= sort_key)
+
+def sort_by_date(sort_list: list[dict], sort_key: bool = True) -> list[dict]:
+    """
+    Функция принимает список словарей
+    и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание).
+    Функция возвращает новый список, отсортированный по дате
+    :param sort_list:список словарей
+    :param sort_key: параметр сортировки (по умолчанию - по убыванию (True))
+    :return: отсортированный по дате список словарей
+    """
+    sorted_list = sorted(sort_list, key=lambda sorted_key: sorted_key["date"][:10], reverse=sort_key)
     return sorted_list
