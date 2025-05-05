1efbd7367a3e23620ac205511e32a50b424657dd
Sergey <bondarevsv36@gmail.com>
2025-05-05 23:26:50 +0300

    исправил ошибку импорта в widget.py изменил название модуля с тестами для модуля masks.py переделал параметризацию на фикстуры

---
+++ tests/test_masks.py
@@ -0,0 +1,61 @@
+import pytest
+
+from src.masks import get_mask_card_number, get_mask_account
+
+
+@pytest.fixture
+def invalid_card_number():
+    return [
+        "12345678901212",
+        "123456789o121234",
+        "",
+        "123456789012345?",
+        "12345678901234  ",
+        "1234 5678 9012 3456"
+    ]
+
+@pytest.fixture
+def valid_card_number():
+    return [
+        ("1234567890121211", "1234 56** **** 1211"),
+        ("0987676543245674", "0987 67** **** 5674"),
+        ("1234567890123456", "1234 56** **** 3456")
+    ]
+
+
+def test_invalid_mask_card_number(invalid_card_number):
+    for number in invalid_card_number:
+        assert get_mask_card_number(number) == "Не верный номер карты"
+
+
+def test_valid_mask_card_number(valid_card_number):
+    for number, expected in valid_card_number:
+        assert get_mask_card_number(number) == expected
+
+@pytest.fixture
+def invalid_account_number():
+    return [
+        "12345678901212",
+        "123456789o1212341212",
+        "",
+        "1234567890123121245?",
+        "123456789011212234  ",
+        "1234 5678 9012 3456 1234"
+    ]
+
+@pytest.fixture
+def valid_account_number():
+    return [
+        ("12345678901234561212", "**1212"),
+        ("65342678901234563445", "**3445"),
+        ("45675688901234563987", "**3987")
+    ]
+
+
+def test_invalid_mask_card_account(invalid_account_number):
+    for number in invalid_account_number:
+        assert get_mask_account(number) == "Не верный номер счета"
+
+def test_valid_account_number(valid_account_number):
+    for number, expected in valid_account_number:
+        assert get_mask_account(number) == expected
