0ce3a03389ad65aa642549a00332e76e9e404129
Sergey <bondarevsv36@gmail.com>
2025-05-08 19:11:46 +0300

    добавлены тесты для модуля widget.py, исправлена логика работы модуля widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,28 +1,70 @@
-from typing import Any
-
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(account_card: str) -> Any:
+def mask_account_card(account_card: str) -> str:
     """
     Функция обработки данных о карте и счете
     """
-    name, number = account_card.rsplit(" ", 1)
-    if name == "Счет":
-        mask = name + " " + get_mask_account(number)
 
+    try:
+        name, number = account_card.rsplit(" ", 1)
+    except ValueError:
+        return 'Ошибка ввода'
+
+    # проверка выбора функции обработки данных
+    # выбор обработки счета и проверки ошибки в номере счета
+    if name.lower() in ["счет", "счёт"] and get_mask_account(number) != 'Ошибка ввода':
+        return name + " " + get_mask_account(number)
+
+    # выбор обработки карты
     else:
-        mask = name + " " + get_mask_card_number(number)
+        # обработка ошибки в номере карты
+        if get_mask_card_number(number) == 'Ошибка ввода':
+            return 'Ошибка ввода'
 
-    return mask
+        # обработка корректного названия карты
+        if name.isalpha():
+            return name + " " + get_mask_card_number(number)
+        # обработка названия карты, состоящего из нескольких слов
+        elif ' ' in name:
+            if ''.join(name.split()).isalpha():
+                return name + " " + get_mask_card_number(number)
+        # вывод ошибки если название карты некорректно
+        else:
+            return 'Ошибка ввода'
+    # если обработка все-таки не произошла
+    return 'Ошибка ввода'
 
 
 def get_date(date_string: str) -> str:
     """
     Функция преобразования формата даты
+    "ГГГГ-ММ-ДД" -> "ДД.ММ.ГГГГ"
     """
+    # Проверка корректности структуры поступающих данных: "XXXX-XX-XX" или "XXXX-XX-X"
+    if len(date_string) < 10:
+        return "Ошибка ввода"
     date_need = date_string[:10]
-    date_list = []
+    if not date_need[-1].isdigit():
+        date_need = date_string[:9]
     date_list = date_need.split("-")
-    sort_date_list = date_list[::-1]
-    return ".".join(sort_date_list)
+    if len(date_list[0]) != 4 or len(date_list[1]) > 2 or len(date_list[2]) >2:
+        return "Ошибка ввода"
+    # Проверка наличия в структуре данных нужного количества разделителей
+    if date_need.count('-') != 2:
+        return "Ошибка ввода"
+    # Проверка наличия в структуре данных трех элементов
+    if len(date_list) != 3:
+        return "Ошибка ввода"
+    # Проверка наличия в структуре данных только цифр
+    for number in date_list:
+        if not number.isdigit():
+            return "Ошибка ввода"
+
+    # Формируем требуемый порядок данных
+    true_date_list = date_list[::-1]
+
+    # Формируем структуру данных
+    true_date = ".".join(true_date_list)
+
+    return true_date
